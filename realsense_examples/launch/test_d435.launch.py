b0af07ba4e9705a02dd37a6176e91647e8ee84f4
ila <inaki.lorente@ageve.net>
2020-06-16 18:50:50 +0200

    launch files

--- realsense_examples/launch/test_d435.launch.py
+++ realsense_examples/launch/test_d435.launch.py
@@ -8,37 +8,21 @@ from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import ThisLaunchFileDir
 
+
 def generate_launch_description():
     # config SN:
-    t265_serial_no = LaunchConfiguration('t265_serial_no', default='905312112031')
     d435_serial_no = LaunchConfiguration('d435_serial_no', default='825312071491')
-    # config
-    t265_conf = os.path.join(get_package_share_directory('realsense_examples'), 'config', 't265.yaml')
+    # config D435
     d435_conf = os.path.join(get_package_share_directory('realsense_examples'), 'config', 'd435.yaml')
 
     return LaunchDescription([
-
-        Node(
-            package='tf2_ros',
-            node_executable='static_transform_publisher',
-            output='screen',
-            arguments=['0', '0', '0.03', '0', '0', '0', 't265_link', 'd435_link']
-            ),
-
-        Node(
-            package='realsense_node',
-            node_executable='realsense_node',
-            node_namespace="/t265",
-            output='screen',
-            parameters=[t265_conf,{'serial_no': t265_serial_no,
-                                   'base_frame_id': 't265_link'}]
-            ),
         Node(
             package='realsense_node',
             node_executable='realsense_node',
             node_namespace="/d435",
             output='screen',
-            parameters=[d435_conf,{'serial_no': d435_serial_no,
+            parameters=[d435_conf,{'serial_no': d435_serial_no, 
                                    'base_frame_id': 'd435_link'}]
             )
     ])
+
